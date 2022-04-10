827cf0650ea1b450747852df690ccd4f74582421
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-04-09 19:19:42 -0600

    Use Haversine distance

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,19 @@ class PlacesController < ApplicationController
 		else
 			qty = 5
 		end
+
 		places.each do |p|
-			d = Math.sqrt( (latitude - p.latitude) ** 2 + (longitude - p.longitude) ** 2 )
+			# Haversine distance
+			lon1 = (longitude/180) * Math::PI  
+    		lon2 = (p['longitude']/180) * Math::PI  
+    		lat1 = (latitude/180) * Math::PI  
+    		lat2 = (p['latitude']/180) * Math::PI  
+      
+    		dlon = lon2 - lon1
+    		dlat = lat2 - lat1
+    		a = Math.sin(dlat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2
+    		d = 2 * Math.asin(Math.sqrt(a)) * 6371
+
 			distances[d] = p
 		end
 
@@ -26,34 +37,14 @@ class PlacesController < ApplicationController
 			item['name']      = v['name']
 			item['latitude']  = v['latitude']
 			item['longitude'] = v['longitude']
-
-			lon1 = (longitude/180) * Math::PI  
-    		lon2 = (v['longitude']/180) * Math::PI  
-    		lat1 = (latitude/180) * Math::PI  
-    		lat2 = (v['latitude']/180) * Math::PI  
-      
-    		dlon = lon2 - lon1
-    		dlat = lat2 - lat1
-    		a = Math.sin(dlat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2
-    		km = 2 * Math.asin(Math.sqrt(a)) * 6371
-
-
-
-			item['distance']  = '%.2f' % km
+			item['regular']   = v['last_regular']
+			item['premium']   = v['last_premium']
+			item['diesel']    = v['last_diesel']
+			item['last_update'] = v['date_update']
+			item['distance']  = '%.2f' % k
 			items[v.id] = item
 			break if c >= qty
 		end
-
-		prices = Price.where("place_id IN (#{items.keys.join(',')})").where(date_add: Price.maximum('date_add'))
-
-		prices.each do |p|
-			items[p.place_id]['regular'] = p.regular
-			items[p.place_id]['premium'] = p.premium
-			items[p.place_id]['diesel'] = p.diesel
-		end
-
-
-		 
 		
 		render json: items.values.to_json
 	end
